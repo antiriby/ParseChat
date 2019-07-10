be1844fefd776d888eae2b1c8dd654424017c5e4
Briana Antiri <antiriby@fb.com>
2019-07-10 10:36:39 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by antiriby on 7/10/19.
+//  Copyright © 2019 antiriby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by antiriby on 7/10/19.
+//  Copyright © 2019 antiriby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
